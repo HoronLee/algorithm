0cefbe2c8cfa0862a50c4696df3be032a087ed10
HoronLee <horonlee@foxmail.com>
2025-11-10 14:09:46 +0800

    两数相加

--- leetcode/1/main.go
+++ leetcode/1/main.go
@@ -1,29 +1,13 @@
-/*
- * 两数之和
- */
 package main
 
-import (
-	"fmt"
-	"slices"
-)
-
-func main() {
-	ret := groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
-	fmt.Printf("%v", ret)
-}
-
-func groupAnagrams(strs []string) [][]string {
-	mp := map[string][]string{}
-	for _, str := range strs {
-		s := []byte(str)
-		slices.Sort(s)
-		sortedStr := string(s)
-		mp[sortedStr] = append(mp[sortedStr], str)
+func twoSum(nums []int, target int) []int {
+	var m = make(map[int]int)
+	for i, num := range nums {
+		diff := target - num
+		if index, isHave := m[diff]; isHave {
+			return []int{index, i}
+		}
+		m[num] = i
 	}
-	ans := make([][]string, 0, len(mp))
-	for _, v := range mp {
-		ans = append(ans, v)
-	}
-	return ans
+	return nil
 }
